4d8d5f4cb0f2ee3a448804a45bb8e78a1e8116f3
arctyra <tyrael@bk.ru>
2020-12-01 23:40:42 +0300

    Added ArrayDeque

--- src/main/java/dto/Host.java
+++ src/main/java/dto/Host.java
@@ -2,16 +2,17 @@ package dto;
 
 import exceptions.DuplicateRequestException;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Host {
     private Set<Request> requestSet = new HashSet<>();
+    private Deque<Request> requestDeque = new ArrayDeque<>();
 
     public void addRequest(Request r) throws DuplicateRequestException {
         if (requestSet.contains(r)) {
             throw new DuplicateRequestException();
         }
         requestSet.add(r);
+        requestDeque.add(r);
     }
 }
